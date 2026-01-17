671b6ad539efe82f75d51592ddb61dfdc3594cb3
maduc <matheusduartec96@gmail.com>
2026-01-17 14:33:31 -0300

    Initial commit

---
+++ src/main/java/com/example/javaSpringProfessional/DesafioCrudDeClientesApplication.java
@@ -0,0 +1,14 @@
+package com.example.javaSpringProfessional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudDeClientesApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(DesafioCrudDeClientesApplication.class, args);
+	}
+
+}
